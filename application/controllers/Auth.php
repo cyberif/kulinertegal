99ea5acd19f3321f25eb32083088301516aa99d2
cyberif <rifqiscout17@gmail.com>
2022-10-19 12:24:35 +0700

    UPLOAD AWAL

---
+++ application/controllers/Auth.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    function index()
+    {
+        $data['title'] = 'Kuliner Tegal - Register';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function register()
+    {
+        $data['title'] = 'Kuliner Tegal - Register';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/register');
+        $this->load->view('templates/auth_footer');
+    }
+}
